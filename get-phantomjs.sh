945bcb521ae6a4860cfe3a9a8191454939fdef2e
Ubuntu <httpd@vm-6858.localdomain>
2017-06-20 12:39:33 +0000

    Vanhempi versio phantomjs:stä.

--- get-phantomjs.sh
+++ get-phantomjs.sh
@@ -3,12 +3,12 @@
 # Download and install phantomjs; the version on Debian doesn't work with the
 # grading scripts, since it's missing phantom.clearCookies(), has some kind of
 # broken require(), and other fun things.
-PHANTOMJS=phantomjs-2.1.1-linux-i686
+PHANTOMJS=phantomjs-1.9.2-linux-i686
 if [ ! -e "$HOME/phantomjs" ]; then
   echo "One moment, downloading PhantomJS..."
   TEMPFILE=$(mktemp)
   TEMPDIR=$(mktemp -d)
-  curl "https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOMJS.tar.bz2" > "$TEMPFILE"
+  curl "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/phantomjs/$PHANTOMJS.tar.bz2" > "$TEMPFILE"
   echo "Unpacking..."
   tar -C "$TEMPDIR" -xjf "$TEMPFILE"
   mv "$TEMPDIR/$PHANTOMJS/bin/phantomjs" "$HOME"
